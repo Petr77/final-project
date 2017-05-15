1a20969323cc7685a092f4dfaf1b304134c06bce
petrsubotnik <Petr77@github.com>
2017-05-15 12:28:54 +1000

    meta added

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,6 @@ class SearchController < ApplicationController
 
   def results
     @locations = Accommodation.near(params[:term], params[:distance], :units => :km).take(20)
-    # @locations.keep_if { |l| l.distance.to_f > 10 }
   end
 
 end
